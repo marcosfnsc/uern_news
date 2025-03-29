d45e5e88f9c5c3153e238b8372f696cdb67ecbb0
Marcos Fonseca <marco12.fonseca@hotmail.com>
2025-03-28 23:18:21 -0300

    criado conexão com o banco

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::{SqlitePool, sqlite::SqliteConnectOptions};
+
+#[tokio::main]
+async fn main() {
+    let poll = SqlitePool::connect_with(
+        SqliteConnectOptions::new()
+            .filename("posts.sqlite")
+            .create_if_missing(true),
+    )
+    .await
+    .unwrap();
 }
